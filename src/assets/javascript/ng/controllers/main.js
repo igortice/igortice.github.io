ae39b7cfceade55edcf7432f28f86167d467f088
Igor Rocha <igortice@gmail.com>
2015-05-26 14:01:07 -0300

    add angular ok

---
+++ src/assets/javascript/ng/controllers/main.js
@@ -0,0 +1,11 @@
+(function () {
+  'use strict';
+
+  angular
+    .module('mainApp')
+    .controller('MainController', MainController);
+
+  function MainController() {
+    var vm = this;
+  }
+}());
