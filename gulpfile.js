198381f6039cc8bb356b87d80e110e76f5c44a49
Igor Rocha <igortice@gmail.com>
2015-06-13 09:51:20 -0300

    add symbol link assets and correct font awesome production

--- gulpfile.js
+++ gulpfile.js
@@ -16,6 +16,11 @@ var reload = browserSync.reload;
 // And define a variable that BrowserSync uses in it"s function
 var bs;
 
+// Symbolik links
+var symlink = require('gulp-sym')
+  , p       = require('path')
+  , File    = require('gulp-util').File;
+
 // Deletes the directory that is used to serve the site during development
 gulp.task("clean:dev", del.bind(null, ["serve"]));
 
@@ -25,7 +30,7 @@ gulp.task("clean:prod", del.bind(null, ["site"]));
 // Runs the build command for Jekyll to compile the site locally
 // This will build the site with the production settings
 gulp.task("jekyll:dev", $.shell.task("jekyll build"));
-gulp.task("jekyll-rebuild", ["jekyll:dev"], function () {
+gulp.task("jekyll-rebuild", ["jekyll:dev"], function (){
   reload;
 });
 
@@ -35,7 +40,7 @@ gulp.task("jekyll-rebuild", ["jekyll:dev"], function () {
 gulp.task("jekyll:prod", $.shell.task("jekyll build --config _config.yml,_config.build.yml"));
 
 // Compiles the SASS files and moves them into the "assets/stylesheets" directory
-gulp.task("styles", function () {
+gulp.task("styles", function (){
   // Looks at the style.scss file for what to include and creates a style.css file
   return gulp.src("src/assets/scss/style.scss")
     .pipe($.sass())
@@ -45,42 +50,42 @@ gulp.task("styles", function () {
     .pipe(gulp.dest("src/assets/stylesheets/"))
     .pipe(gulp.dest("serve/assets/stylesheets/"))
     // Outputs the size of the CSS file
-    .pipe($.size({title: "styles"}))
+    .pipe($.size({ title: "styles" }))
     // Injects the CSS changes to your browser since Jekyll doesn"t rebuild the CSS
-    .pipe(reload({stream: true}));
+    .pipe(reload({ stream: true }));
 });
 
 // Optimizes the images that exists
-gulp.task("images", function () {
+gulp.task("images", function (){
   return gulp.src("src/assets/images/**")
     .pipe($.changed("site/assets/images"))
     .pipe($.imagemin({
       // Lossless conversion to progressive JPGs
       progressive: true,
       // Interlace GIFs for progressive rendering
-      interlaced: true
+      interlaced:  true
     }))
     .pipe(gulp.dest("site/assets/images"))
-    .pipe($.size({title: "images"}));
+    .pipe($.size({ title: "images" }));
 });
 
 // Copy over fonts to the "site" directory
-gulp.task("fonts", function () {
+gulp.task("fonts", function (){
   return gulp.src("src/assets/fonts/**")
     .pipe(gulp.dest("site/assets/fonts"))
     .pipe($.size({ title: "fonts" }));
 });
 
 // Copy xml and txt files to the "site" directory
-gulp.task("copy", function () {
+gulp.task("copy", function (){
   return gulp.src(["serve/*.txt", "serve/*.xml"])
     .pipe(gulp.dest("site"))
     .pipe($.size({ title: "xml & txt" }))
 });
 
 // Optimizes all the CSS, HTML and concats the JS etc
-gulp.task("html", ["styles"], function () {
-  var assets = $.useref.assets({searchPath: "serve"});
+gulp.task("html", ["styles"], function (){
+  var assets = $.useref.assets({ searchPath: "serve" });
 
   return gulp.src("serve/**/*.html")
     .pipe(assets)
@@ -89,7 +94,7 @@ gulp.task("html", ["styles"], function () {
     // Minify CSS
     .pipe($.if("*.css", $.minifyCss()))
     // Start cache busting the files
-    .pipe($.revAll({ ignore: [".eot", ".svg", ".ttf", ".woff"] }))
+    .pipe($.revAll({ ignore: [".eot", ".svg", ".ttf", ".woff", "woff2"] }))
     .pipe(assets.restore())
     // Conctenate your files based on what you specified in _layout/header.html
     .pipe($.useref())
@@ -97,33 +102,32 @@ gulp.task("html", ["styles"], function () {
     .pipe($.revReplace())
     // Minify HTML
     .pipe($.if("*.html", $.htmlmin({
-      removeComments: true,
-      removeCommentsFromCDATA: true,
+      removeComments:               true,
+      removeCommentsFromCDATA:      true,
       removeCDATASectionsFromCDATA: true,
-      collapseWhitespace: true,
-      collapseBooleanAttributes: true,
-      removeAttributeQuotes: true,
-      removeRedundantAttributes: true
+      collapseWhitespace:           true,
+      collapseBooleanAttributes:    true,
+      removeAttributeQuotes:        true,
+      removeRedundantAttributes:    true
     })))
     // Send the output to the correct folder
     .pipe(gulp.dest("site"))
-    .pipe($.size({title: "optimizations"}));
+    .pipe($.size({ title: "optimizations" }));
 });
 
-
 // Task to upload your site to your personal GH Pages repo
-gulp.task("deploy", function () {
+gulp.task("deploy", function (){
   // Deploys your optimized site, you can change the settings in the html task if you want to
   return gulp.src("./site/**/*")
     .pipe($.ghPages({
       // Currently only personal GitHub Pages are supported so it will upload to the master
       // branch and automatically overwrite anything that is in the directory
       branch: "master"
-      }));
+    }));
 });
 
 // Run JS Lint against your JS
-gulp.task("jslint", function () {
+gulp.task("jslint", function (){
   gulp.src("./serve/assets/javascript/*.js")
     // Checks your JS code quality against your .jshintrc file
     .pipe($.jshint(".jshintrc"))
@@ -137,7 +141,7 @@ gulp.task("doctor", $.shell.task("jekyll doctor"));
 // BrowserSync will serve our site on a local server for us and other devices to use
 // It will also autoreload across all devices as well as keep the viewport synchronized
 // between them.
-gulp.task("serve:dev", ["styles", "jekyll:dev"], function () {
+gulp.task("serve:dev", ["styles", "jekyll:dev"], function (){
   bs = browserSync({
     notify: true,
     // tunnel: "",
@@ -149,14 +153,14 @@ gulp.task("serve:dev", ["styles", "jekyll:dev"], function () {
 
 // These tasks will look for files that change while serving and will auto-regenerate or
 // reload the website accordingly. Update or add other files you need to be watched.
-gulp.task("watch", function () {
+gulp.task("watch", function (){
   gulp.watch(["src/**/*.md", "src/**/*.html", "src/**/*.xml", "src/**/*.txt", "src/**/*.js"], ["jekyll-rebuild"]);
   gulp.watch(["serve/assets/stylesheets/*.css"], reload);
   gulp.watch(["src/assets/scss/**/*.scss"], ["styles"]);
 });
 
 // Serve the site after optimizations to see that everything looks fine
-gulp.task("serve:prod", function () {
+gulp.task("serve:prod", function (){
   bs = browserSync({
     notify: false,
     // tunnel: true,
@@ -166,19 +170,57 @@ gulp.task("serve:prod", function () {
   });
 });
 
+gulp.task("symlink:dev", function (){
+  // Link CSS
+  gulp
+    .src([
+      'src/_bower_components/foundation/css/normalize.css',
+      'src/_bower_components/foundation/css/foundation.css',
+      'src/_bower_components/font-awesome/css/font-awesome.css',
+      'src/_bower_components/animsition/dist/css/animsition.css'
+    ])
+    .pipe(symlink(function (source){
+      return p.resolve('src/assets/stylesheets', p.basename(source.path))
+    }, { force: true }));
+
+  // Link FONTS
+  gulp
+    .src([
+      'src/_bower_components/font-awesome/fonts/*'
+    ])
+    .pipe(symlink(function (source){
+      return p.resolve('src/assets/fonts', p.basename(source.path))
+    }, { force: true }));
+
+  // Link JS
+  gulp
+    .src([
+      'src/_bower_components/modernizr/modernizr.js',
+      'src/_bower_components/jquery/dist/jquery.js',
+      'src/_bower_components/foundation/js/foundation.js',
+      'src/_bower_components/angular/angular.js',
+      'src/_bower_components/angular-gravatar/build/angular-gravatar.js',
+      'src/_bower_components/animsition/dist/js/jquery.animsition.js'
+    ])
+    .pipe(symlink(function (source){
+      return p.resolve('src/assets/javascript', p.basename(source.path))
+    }, { force: true }));
+});
+
 // Default task, run when just writing "gulp" in the terminal
 gulp.task("default", ["serve:dev", "watch"]);
 
 // Checks your CSS, JS and Jekyll for errors
-gulp.task("check", ["jslint", "doctor"], function () {
+gulp.task("check", ["jslint", "doctor"], function (){
   // Better hope nothing is wrong.
 });
 
 // Builds the site but doesn"t serve it to you
-gulp.task("build", ["jekyll:prod", "styles"], function () {});
+gulp.task("build", ["jekyll:prod", "styles"], function (){
+});
 
 // Builds your site with the "build" command and then runs all the optimizations on
 // it and outputs it to "./site"
-gulp.task("publish", ["build"], function () {
+gulp.task("publish", ["build"], function (){
   gulp.start("html", "copy", "images", "fonts");
 });
---
+++ src/_drafts/comandos-basicos-git.md
@@ -0,0 +1,32 @@
+---
+layout: post
+date: 2015-06-06
+title: Comandos Básicos Git
+permalink: /:categories/:title.html
+categories: git
+tags: git
+author: igor_rocha
+desc: "Comandos Básicos Git"
+keywords: "git, comandos git, github, git init"
+---
+
+<dl class="accordion" data-accordion>
+  <dd class="accordion-navigation">
+    <a href="#panel1b"><strong> - git init</strong></a>
+    <div id="panel1b" class="content">
+      Inicializar um repositório Git vazio
+    </div>
+  </dd>
+  <dd class="accordion-navigation">
+    <a href="#panel2b"><strong> - git status</strong></a>
+    <div id="panel2b" class="content">
+      Mostrar status dos arquivos comparando-os com o seu commit HEAD
+    </div>
+  </dd>
+  <dd class="accordion-navigation">
+    <a href="#panel3b">Accordion 3</a>
+    <div id="panel3b" class="content">
+      Panel 3. Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+    </div>
+  </dd>
+</dl>
